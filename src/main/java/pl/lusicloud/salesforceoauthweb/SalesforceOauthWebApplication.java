687695589dfb0711ab7d9b27129b41f16a17435c
dnldaniel <ttt.daniel@gmail.com>
2026-08-06 19:59:11 +0200

    Empty spring project - absolutely no logic

---
+++ src/main/java/pl/lusicloud/salesforceoauthweb/SalesforceOauthWebApplication.java
@@ -0,0 +1,13 @@
+package pl.lusicloud.salesforceoauthweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceOauthWebApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(SalesforceOauthWebApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/lusicloud/salesforceoauthweb/SalesforceOauthWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lusicloud.salesforceoauthweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesforceOauthWebApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
