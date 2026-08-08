2bd3088f89e3cc789405663d0f3297d54cc53fd9
dnldaniel <ttt.daniel@gmail.com>
2026-08-08 17:30:25 +0200

    Add SalesforceAuthenticationException

---
+++ src/main/java/pl/lusicloud/salesforceoauthweb/SalesforceAuthenticationException.java
@@ -0,0 +1,8 @@
+package pl.lusicloud.salesforceoauthweb;
+
+public final class SalesforceAuthenticationException extends RuntimeException {
+
+  public SalesforceAuthenticationException(String message) {
+    super(message);
+  }
+}
